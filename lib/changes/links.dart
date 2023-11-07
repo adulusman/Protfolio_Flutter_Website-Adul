e239f2346ef8934015b0c9a4320feb2b27969fb2
adulusman <adulusman@gmail.com>
2023-11-07 15:49:08 +0530

    new

---
+++ lib/changes/links.dart
@@ -0,0 +1,36 @@
+// CV
+const String resume = 'https://github.com/adulusman';
+
+// Github Link
+const String gitHub = 'https://github.com/adulusman';
+// Github Link
+const String facebook = 'https://www.facebook.com/adul.usman.9';
+// Github Link
+const String twitter = 'https://twitter.com/adoooozz123';
+// Github Link
+const String youtube = 'https://youtube.com/@muslic_lyrics';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/adul-m-9394081b2/';
+// Github Link
+const String buymeacoffee = 'https://www.buymeacoffee.com/sudeshnb';
+// Github Link
+const String instagram = 'https://www.instagram.com/adul_m_/';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/sudeshnb",
+//   // "https://instagram.com/sudesh_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/sudesh78",
+//   "https://linkedin.com/in/sudeshnb",
+//   "https://www.buymeacoffee.com/sudeshnb", "https://github.com/sudeshnb",
+// ];
+//
+const String profileImage =
+    "https://avatars.githubusercontent.com/u/70052111?v=4";
+//
+// Github Link
+const String fiverr = 'https://www.fiverr.com/adul_m?up_rollout=true';
+// Github Link
+const String upwork = 'https://www.upwork.com/freelancers/~01facf102acad8dbb5';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=7306121517';
